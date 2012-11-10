47589d13ab230271796b1c467b80f5f4e6ebf004
Devin Schwab <digidevin@gmail.com>
2012-11-09 23:26:54 -0500

    Added basic skeleton of a class to represent a learning unit in the scenario

---
+++ src/edu/cwru/sepia/agent/LearningUnit.java
@@ -0,0 +1,137 @@
+package edu.cwru.sepia.agent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.environment.model.history.History.HistoryView;
+import edu.cwru.sepia.environment.model.state.State;
+import edu.cwru.sepia.environment.model.state.State.StateView;
+
+public class LearningUnit {
+	
+	public final int unitId;
+	
+	final double alpha = .1; // learning rate
+	final double beta = .1; 
+
+	double e = 0; // eligibility trace
+	
+	List<Feature> features;
+	double[] weights;
+	
+	
+	public LearningUnit(int unitId)
+	{
+		this.unitId = unitId;
+		
+		features = new ArrayList<Feature>();
+		
+		// add features here
+		features.add(new IsClosestEnemy());
+		features.add(new NumFootmenAttackingEnemy());
+		features.add(new EnemyType());
+		features.add(new ClosestTowerDistance());
+		features.add(new IsEnemyAttackingMe());
+		features.add(new HitPointsRatio());
+		features.add(new ClosestBallistaDistance());
+		
+		weights = new double[features.size() + 1];
+	}
+	
+	public void updateWeights(double reward)
+	{
+		// theta + alpha * reward * e
+		e = calculateE();
+	}
+	
+	private double calculateE()
+	{
+		// beta*e + grad log Pr(a | s, theta)
+		return 0.0;
+	}
+	
+	public Action getAction(StateView state)
+	{
+		// follow current policy
+		return null;
+	}
+
+	private static class IsClosestEnemy implements Feature
+	{
+
+		@Override
+		public double calculate(StateView s, HistoryView log, Action a) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	private static class NumFootmenAttackingEnemy implements Feature
+	{
+
+		@Override
+		public double calculate(StateView s, HistoryView log, Action a) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	private static class EnemyType implements Feature
+	{
+
+		@Override
+		public double calculate(StateView s, HistoryView log, Action a) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	private static class ClosestTowerDistance implements Feature
+	{
+
+		@Override
+		public double calculate(StateView s, HistoryView log, Action a) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	private static class IsEnemyAttackingMe implements Feature
+	{
+
+		@Override
+		public double calculate(StateView s, HistoryView log, Action a) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	private static class HitPointsRatio implements Feature
+	{
+
+		@Override
+		public double calculate(StateView s, HistoryView log, Action a) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	private static class ClosestBallistaDistance implements Feature
+	{
+
+		@Override
+		public double calculate(StateView s, HistoryView log, Action a) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+
+}
